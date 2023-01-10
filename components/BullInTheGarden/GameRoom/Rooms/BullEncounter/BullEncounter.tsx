489d2e7fac3a8ffb1a32a40ef116ae718fd737ec
christopher coyne <christcoyne@gmail.com>
2023-01-09 23:02:10 -0500

    bull encounter now includes final video

--- components/BullInTheGarden/GameRoom/Rooms/BullEncounter/BullEncounter.tsx
+++ components/BullInTheGarden/GameRoom/Rooms/BullEncounter/BullEncounter.tsx
@@ -1,37 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./BullEcounter.module.css";
 import { Props } from "./constants";
 import Image from "next/image";
 
-export default function BullEncounter({ iterations }: Props) {
+export default function BullEncounter({ iterations, setFullscreen }: Props) {
   const [background, setBackground] = useState("bull-1.png");
   const backgroundUrl = `/bull-in-the-garden/bull/${background}`;
+
+  useEffect(() => {
+    if (background === "bull-distort1.png") {
+      console.log("set full screen");
+      setFullscreen(true);
+    }
+    return () => {
+      setFullscreen(false);
+    };
+  }, [background, setFullscreen]);
 
   if (background === "bull-1.png") {
     setTimeout(() => setBackground("bull-mutated.png"), 1000);
   } else {
     if (background === "bull-mutated.png") {
       setTimeout(() => setBackground("bull-distort1.png"), 1000);
-    } else {
-      setTimeout(() => {
-        if (background === "bull-distort1.png") {
-          setBackground("bull-distort2.png");
-        } else {
-          setBackground("bull-distort1.png");
-        }
-      }, 100);
     }
   }
   console.log("url ", backgroundUrl);
   return (
     <div className={styles.container}>
-      <Image src={backgroundUrl} alt="bull" width="500" height="300" />
+      {background === "bull-distort1.png" ? (
+        <video autoPlay muted width="100%" height="100%" loop>
+          <source
+            src="/bull-in-the-garden/bull/final_bull.mp4"
+            type="video/mp4"
+          />
+        </video>
+      ) : (
+        <Image src={backgroundUrl} alt="bull" width="500" height="300" />
+      )}
     </div>
   );
 }
-
-/*
-      style={{
-        backgroundImage: `url("${backgroundUrl}")`,
-      }}
-      */
