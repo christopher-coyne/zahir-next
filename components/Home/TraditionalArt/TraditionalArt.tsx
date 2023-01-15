71b9d071a88034958a963602314d0721dd89b1c9
christopher coyne <christcoyne@gmail.com>
2023-01-15 14:18:41 -0500

    made responsive

--- components/Home/TraditionalArt/TraditionalArt.tsx
+++ components/Home/TraditionalArt/TraditionalArt.tsx
@@ -4,29 +4,41 @@ import Link from "next/link";
 import { TypeFlags } from "typescript";
 export const TraditionalArt = () => {
   const thumbnails = [
-    "Falling Through",
-    "Creation",
-    "Birds and Fishes",
-    "The Angel",
-    "Prospero",
-    "Palms",
+    {
+      title: "Falling Through",
+      link: "https://www.instagram.com/p/Cke166WgvYd/",
+    },
+    { title: "Creation", link: "https://www.instagram.com/p/CkezxvDASsW/" },
+    {
+      title: "Birds and Fishes",
+      link: "https://www.instagram.com/p/CkeyvZogKM9/",
+    },
+    { title: "The Angel", link: "https://www.instagram.com/p/Cke166WgvYd/" },
+    { title: "Prospero", link: "https://www.instagram.com/p/Cke1Esrgmms/" },
+    { title: "Palms", link: "https://www.instagram.com/p/CkHrv8TL5wo/" },
+    { title: "Bull", link: "https://www.instagram.com/p/CkezFR0gY-a/" },
+    {
+      title: "Remain In Light",
+      link: "https://www.instagram.com/p/Cke0h-FgdBT/",
+    },
+    { title: "Memory", link: "https://www.instagram.com/p/CkezxvDASsW/" },
   ];
   return (
     <div className={styles.gridContainer}>
-      {thumbnails.map((name) => (
-        <div className={styles.thumbnailContainer} key={name}>
+      {thumbnails.map((thumbnail) => (
+        <div className={styles.thumbnailContainer} key={thumbnail.title}>
           <div className={styles.imgContainer}>
-            <Link href={`/artwork/${name}`}>
+            <a href={thumbnail.link} target="_blank" rel="noreferrer">
               <Image
-                src={`/thumbnails/${name}.png`}
-                alt={name}
+                src={`/thumbnails/${thumbnail.title}.png`}
+                alt={thumbnail.title}
                 fill
                 className={styles.img}
               />
-            </Link>
+            </a>
           </div>
           <div className={styles.titleContainer}>
-            <h4 className={styles.title}>{name}</h4>
+            <h4 className={styles.title}>{thumbnail.title}</h4>
           </div>
         </div>
       ))}
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./Navbar.module.css";
+import styles from "./Layout.module.css";
 import { Navbar } from "./Navbar";
 
 type Props = {
